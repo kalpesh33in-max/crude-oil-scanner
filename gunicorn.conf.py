4e06ce346e70f6262c942b1c743f7e9bad2b8687
kalpesh33in-max <kalpesh33in@gmail.com>
2025-12-09 21:13:00 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,2 @@
-# gunicorn.conf.py
-
-# Set the number of worker processes to 1. 
-# This is CRITICAL to prevent Gunicorn from fighting with the background thread's asyncio loop.
 workers = 1 
-
-# Set the number of threads per worker to 1 (optional, but good practice for stability).
 threads = 1
